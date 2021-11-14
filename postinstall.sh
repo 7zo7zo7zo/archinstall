72f8329c4308e4b4c69504a247828affa8d3047d
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2021-11-14 14:07:52 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,13 +1,11 @@
-pacman --noconfirm -S linux-headers neofetch htop xorg-server xorg-xint libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl
-git clone https://github.com/7zo7zo7zo/dwm-main.git temp
-mv temp/dwm-main temp/dwm
+sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xint libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl
+git clone https://github.com/7zo7zo7zo/dwm-main.git temp/dwm
 mv temp/dwm ~/.config
-git clone https://github.com/7zo7zo7zo/st-main.git ~/.config
-mv temp/st-main temp/st
+git clone https://github.com/7zo7zo7zo/st-main.git temp/st
 mv temp/st ~/.config
-git clone https://github.com/7zo7zo7zo/slstatus.git ~/.config
+git clone https://github.com/7zo7zo7zo/slstatus.git temp/slstatus
 mv temp/slstatus ~/.config
-git clone https://github.com/7zo7zo7zo/dmenu.git ~/.config
+git clone https://github.com/7zo7zo7zo/dmenu.git temp/dmenu
 mv temp/dmenu ~/.config
 rm -rf temp
 cd ~/.config/dwm ; sudo make clean install
