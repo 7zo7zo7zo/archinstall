0cb2eb4147420441e8eebe0799fa9ba839e7e09d
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2021-11-13 19:22:32 -0800

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -18,37 +18,4 @@ fi
 mount $partition /mnt 
 pacstrap /mnt base base-devel linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "KEYMAP=us" > /etc/vconsole.conf
-echo "Hostname: "
-read hostname
-echo $hostname > /etc/hostname
-echo "127.0.0.1       localhost" >> /etc/hosts
-echo "::1             localhost" >> /etc/hosts
-echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
-mkinitcpio -P
-passwd
-pacman --noconfirm -S grub efibootmgr os-prober networkmanager
-echo "Enter EFI partition: " 
-read efipartition
-mkdir /boot/efi
-mount $efipartition /boot/efi 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-#sed -i 's/quiet/pci=noaer/g' /etc/default/grub
-#sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-systemctl enable NetworkManager.service
-//echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo "Enter Username: "
-read username
-useradd -m -G wheel,audio,video -s /bin/bash $username
-passwd $username
-exit
-umount -R /mnt
-reboot
+arch-chroot /mnt ./archinstall2.sh
