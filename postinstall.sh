f5ffc8f967bd4cd653c92a1cbd3300fc6b9c4ec7
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2021-11-14 14:16:12 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,4 @@
-sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xint libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl
+sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xinit libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl
 git clone https://github.com/7zo7zo7zo/dwm-main.git temp/dwm
 mv temp/dwm ~/.config
 git clone https://github.com/7zo7zo7zo/st-main.git temp/st
