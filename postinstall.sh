e96cf3b4119c1e255f82ad82507b60d29ccccf36
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2021-11-14 14:01:27 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,15 @@
 pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xint libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl
-git clone https://github.com/7zo7zo7zo/dwm-main.git ~/.config
+git clone https://github.com/7zo7zo7zo/dwm-main.git temp
+mv temp/dwm-main temp/dwm
+mv temp/dwm ~/.config
 git clone https://github.com/7zo7zo7zo/st-main.git ~/.config
+mv temp/st-main temp/st
+mv temp/st ~/.config
 git clone https://github.com/7zo7zo7zo/slstatus.git ~/.config
+mv temp/slstatus ~/.config
 git clone https://github.com/7zo7zo7zo/dmenu.git ~/.config
-mv ~/.config/dwm-main ~/.config/dwm
-mv ~/.config/st-main ~/.config/st
+mv temp/dmenu ~/.config
+rm -rf temp
 cd ~/.config/dwm ; sudo make clean install
 cd ~/.config/st ; sudo make clean install
 cd ~/.config/slstatus ; sudo make clean install
