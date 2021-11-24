c7be4bd69342d053882ad5c37e7524f527546d0d
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2021-11-24 00:54:13 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,15 +1,11 @@
 sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xinit libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl pulseaudio man-db pavucontrol ttf-droid picom
-mkdir temp
+#mkdir temp
 git clone https://github.com/7zo7zo7zo/dwm-main.git temp/dwm
-mv temp/dwm ~/.config
 git clone https://github.com/7zo7zo7zo/st-main.git temp/st
-mv temp/st ~/.config
 git clone https://github.com/7zo7zo7zo/slstatus.git temp/slstatus
-mv temp/slstatus ~/.config
 git clone https://github.com/7zo7zo7zo/dmenu.git temp/dmenu
-mv temp/dmenu ~/.config
 #git clone https://github.com/7zo7zo7zo/xmenu.git temp/xmenu
-#mv temp/xmenu ~/.config
+mv temp/* ~/.config
 cd ~/.config/dwm ; sudo make clean install
 cd ~/.config/st ; sudo make clean install
 cd ~/.config/slstatus ; sudo make clean install
@@ -17,7 +13,7 @@ cd ~/.config/dmenu ; sudo make clean install
 #cd ~/.config/xmenu ; sudo make clean install
 git clone https://aur.archlinux.org/yay.git temp/yay
 cd temp/yay ; makepkg -si --noconfirm
-systemctl --user enable pulseaudio
+systemctl --user enable --now pulseaudio
 git clone http://github.com/7zo7zo7zo/dotfiles temp/dotfiles
 rm -rf temp/dotfiles/.git
 #sudo mv temp/dotfiles/pacman.conf /etc
