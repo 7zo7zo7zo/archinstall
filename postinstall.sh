5287ca402bf33baa049cb41bb7de8e519e0f0ce5
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-01-15 21:53:19 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,4 @@
-sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xinit libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl pulseaudio man-db pavucontrol ttf-droid picom ttf-liberation feh python-pywal zathura zathura-pdf-poppler qpdf cups unzip
+sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xinit libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl pulseaudio man-db pavucontrol ttf-droid picom ttf-liberation feh python-pywal zathura zathura-pdf-poppler qpdf cups unzip man-pages
 git clone https://github.com/7zo7zo7zo/dwm-main.git temp/dwm
 git clone https://github.com/7zo7zo7zo/st-main.git temp/st
 git clone https://github.com/7zo7zo7zo/slstatus.git temp/slstatus
