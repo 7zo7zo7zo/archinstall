d19ed675d1e9d5307d42793195947027748f78e4
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-02-09 00:11:20 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,13 @@
 sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xinit libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl pulseaudio man-db pavucontrol ttf-droid picom feh python-pywal zathura zathura-pdf-mupdf poppler-utils qpdf cups unzip man-pages ttf-nerd-fonts-symbols gimp
-#sudo pacman --noconfirm --needed -S xcolor xorg-xfd xorg-bdftopcf zathura-pdf-poppler ttf-font-awesome intellij-idea-community-edition alsa-utils xf86-input-synaptics nvidia nvidia-dkms ttf-liberation pamixer lxappearance hplip
-git clone https://github.com/7zo7zo7zo/dwm-main.git temp/dwm
+#sudo pacman --noconfirm --needed -S xcolor xorg-xfd xorg-bdftopcf zathura-pdf-poppler ttf-font-awesome intellij-idea-community-edition alsa-utils xf86-input-synaptics nvidia nvidia-dkms ttf-liberation pamixer lxappearance hplip openjdk11-src
+git clone https://github.com/7zo7zo7zo/dwm-testing.git temp/dwm
 git clone https://github.com/7zo7zo7zo/st-main.git temp/st
 git clone https://github.com/7zo7zo7zo/dwmblocks.git temp/dwmblocks
 git clone https://github.com/7zo7zo7zo/dmenu.git temp/dmenu
 #git clone https://github.com/7zo7zo7zo/xmenu.git temp/xmenu
 mkdir ~/.config
 mv temp/* ~/.config
+#git clone https://github.com/phillbush/xclickroot.git temp/xclickroot ; cd temp/xclickroot ; sudo make clean install
 cd ~/.config/dwm ; sudo make clean install
 cd ~/.config/st ; sudo make clean install
 cd ~/.config/slstatus ; sudo make clean install
