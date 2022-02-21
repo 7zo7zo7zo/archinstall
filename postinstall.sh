48c4eda2a5815e0e25f68a1259452f5bf9bcd1a3
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-02-21 11:20:33 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,19 +1,15 @@
-sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xinit libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl pulseaudio man-db pavucontrol ttf-droid picom feh python-pywal zathura zathura-pdf-mupdf poppler-utils qpdf cups unzip man-pages ttf-nerd-fonts-symbols gimp
-#sudo pacman --noconfirm --needed -S xcolor xorg-xfd xorg-bdftopcf zathura-pdf-poppler ttf-font-awesome intellij-idea-community-edition alsa-utils xf86-input-synaptics nvidia nvidia-dkms ttf-liberation pamixer lxappearance hplip openjdk11-src unrar
-git clone https://github.com/7zo7zo7zo/dwm-testing.git temp/dwm
-git clone https://github.com/7zo7zo7zo/st-main.git temp/st
+sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xinit libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl pulseaudio man-db pavucontrol ttf-droid picom zathura zathura-pdf-mupdf poppler-utils qpdf cups unzip man-pages ttf-nerd-fonts-symbols gimp
+#sudo pacman --noconfirm --needed -S xcolor xorg-xfd xorg-bdftopcf zathura-pdf-poppler ttf-font-awesome intellij-idea-community-edition alsa-utils xf86-input-synaptics nvidia nvidia-dkms ttf-liberation pamixer lxappearance hplip openjdk11-src unrar xorg-xprop feh python-pywal
+git clone https://github.com/7zo7zo7zo/dwm.git temp/dwm
+git clone https://github.com/7zo7zo7zo/st.git temp/st
 git clone https://github.com/7zo7zo7zo/dwmblocks.git temp/dwmblocks
-git clone https://github.com/7zo7zo7zo/dmenu.git temp/dmenu
-#git clone https://github.com/7zo7zo7zo/xmenu.git temp/xmenu
-#git clone https://github.com/phillbush/xclickroot.git temp/xclickroot
+git clone https://git.suckless.org/dmenu temp/dmenu
 mkdir ~/.config
 mv temp/* ~/.config
 cd ~/.config/dwm ; sudo make clean install
 cd ~/.config/st ; sudo make clean install
 cd ~/.config/slstatus ; sudo make clean install
 cd ~/.config/dmenu ; sudo make clean install
-#cd ~/.config/xmenu ; sudo make clean install
-#cd ~/.config/xclickroot ; sudo make clean install
 systemctl --user enable --now pulseaudio
 sudo systemctl enable --now cups
 #git clone https://aur.archlinux.org/yay.git temp/yay
