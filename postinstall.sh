c3f26dd7d813d91163edff03581daf1c3fa7d8e6
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-04-07 12:25:14 -0700

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
 sudo pacman --noconfirm --needed -S linux-headers neofetch htop xorg-server xorg-xinit libxft libxinerama xorg-xrandr ttf-jetbrains-mono gnu-free-fonts firefox mpv openssh git wget vim neovim youtube-dl pulseaudio man-db pavucontrol ttf-droid picom zathura zathura-pdf-mupdf poppler-utils qpdf cups unzip man-pages ttf-nerd-fonts-symbols gimp
-#sudo pacman --noconfirm --needed -S xcolor xorg-xfd xorg-bdftopcf zathura-pdf-poppler ttf-font-awesome intellij-idea-community-edition alsa-utils xf86-input-synaptics nvidia nvidia-dkms ttf-liberation pamixer lxappearance hplip openjdk11-src unrar xorg-xprop feh python-pywal ttf-fira-code libimobiledevice godot
+#sudo pacman --noconfirm --needed -S xcolor xorg-xfd xorg-bdftopcf zathura-pdf-poppler ttf-font-awesome intellij-idea-community-edition alsa-utils xf86-input-synaptics nvidia nvidia-dkms ttf-liberation pamixer lxappearance hplip openjdk11-src unrar xorg-xprop feh python-pywal ttf-fira-code libimobiledevice godot screenkey
 git clone https://github.com/7zo7zo7zo/dwm.git temp/dwm
 git clone https://github.com/7zo7zo7zo/st.git temp/st
 git clone https://github.com/7zo7zo7zo/dwmblocks.git temp/dwmblocks
