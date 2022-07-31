2caa47724da58b8fe6983c3ce69aff0153cdefb6
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-07-31 01:30:33 -0700

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-sudo pacman --noconfirm --needed -S $(< packages)
-#sudo pacman --noconfirm --needed -S $(< optional_packages)
-git clone https://github.com/7zo7zo7zo/dwm.git temp/dwm
-git clone https://github.com/7zo7zo7zo/st.git temp/st
-git clone https://github.com/7zo7zo7zo/dwmblocks.git temp/dwmblocks
-git clone https://git.suckless.org/dmenu temp/dmenu
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+pacman --noconfirm --needed -S $(< packages)
+#pacman --noconfirm --needed -S $(< optional_packages)
 mkdir ~/.config
-mv temp/* ~/.config
-cd ~/.config/dwm ; sudo make clean install
-cd ~/.config/st ; sudo make clean install
-cd ~/.config/slstatus ; sudo make clean install
-cd ~/.config/dmenu ; sudo make clean install
+git clone https://github.com/7zo7zo7zo/dwm.git ~/.config/dwm
+git clone https://github.com/7zo7zo7zo/st.git ~/.config/st
+git clone https://github.com/7zo7zo7zo/dwmblocks.git ~/.config/dwmblocks
+git clone https://git.suckless.org/dmenu.git ~/.config/dmenu
+git clone https://codeberg.org/nsxiv/nsxiv.git ~/.config/nsxiv
+cd ~/.config/dwm ; make clean install
+cd ~/.config/st ; make clean install
+cd ~/.config/dmenu ; make clean install
+cd ~/.config/nsxiv ; make clean install
 systemctl --user enable --now pulseaudio
-sudo systemctl enable --now cups
-#git clone https://aur.archlinux.org/yay.git temp/yay
+systemctl enable --now cups
+TMP=$(mktemp -d)
+#git clone https://aur.archlinux.org/yay.git $TMP/yay
 #cd temp/yay ; makepkg -si --noconfirm
-git clone https://aur.archlinux.org/paru.git temp/paru
+git clone https://aur.archlinux.org/paru.git $TMP/paru
 cd temp/paru ; makepkg -si --noconfirm
-git clone http://github.com/7zo7zo7zo/dotfiles temp/dotfiles
-rm -rf temp/dotfiles/.git
-#sudo mv temp/dotfiles/pacman.conf /etc
-#sudo mv temp/dotfiles/xorg.conf /etc/X11
-cp -r temp/dotfiles/. ~
-rm -rf ./temp
+git clone http://github.com/7zo7zo7zo/dotfiles $TMP/dotfiles
+rm -rf $TMP/dotfiles/.git
+#mv $TMP/dotfiles/pacman.conf /etc
+#mv $TMP/dotfiles/xorg.conf /etc/X11
+cp -r $TMP/dotfiles/. ~
+rm -rf $TMP
